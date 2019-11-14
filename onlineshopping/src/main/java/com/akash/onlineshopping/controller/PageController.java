01890c3de3d2597eee236d8399a847b31034244a
akashdixit <akashdixit25@gmail.com>
2019-11-14 16:15:27 +0530

    Second Push

---
+++ onlineshopping/src/main/java/com/akash/onlineshopping/controller/PageController.java
@@ -0,0 +1,45 @@
+package com.akash.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Controller
+@EnableWebMvc
+public class PageController {
+	
+	@RequestMapping(value= {"/","/home","/index"})
+	public ModelAndView index() {
+		
+		ModelAndView mv =new ModelAndView("page");
+		mv.addObject("greeting","Welcome to online shopping");
+		return mv;
+		
+	}
+	/*@RequestMapping(value= {"/test"})
+	public ModelAndView test(@RequestParam (value="greeting", required=false) String greeting) {
+		if(greeting==null) {
+			greeting ="Hello There!!";
+		}
+		ModelAndView mv =new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+		
+	}*/
+	
+	
+	
+	@RequestMapping("/test/{greeting}")
+	public ModelAndView test(@PathVariable String greeting) {
+	//	String greeting="bc";	
+		ModelAndView mv =new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+		
+	}
+
+}
