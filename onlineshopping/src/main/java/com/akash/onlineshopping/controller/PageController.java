ceb77a3d579e4c3e6a3320a9676f56f8730055c3
akashdixit <akashdixit25@gmail.com>
2019-11-15 11:40:27 +0530

    PArt 2 completed

--- onlineshopping/src/main/java/com/akash/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/akash/onlineshopping/controller/PageController.java
@@ -1,45 +1,34 @@
 package com.akash.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
-@EnableWebMvc
 public class PageController {
 	
-	@RequestMapping(value= {"/","/home","/index"})
-	public ModelAndView index() {
-		
-		ModelAndView mv =new ModelAndView("page");
-		mv.addObject("greeting","Welcome to online shopping");
-		return mv;
-		
-	}
-	@RequestMapping(value= {"/test"})
-	public ModelAndView test(@RequestParam (value="greeting", required=false) String greeting) {
-		if(greeting==null) {
-			greeting ="Hello There!!";
-		}
-		ModelAndView mv =new ModelAndView("page");
-		mv.addObject("greeting",greeting);
-		return mv;
-		
+	@RequestMapping(value = {"/", "/home", "/index"})
+	public ModelAndView index() {		
+		ModelAndView mv = new ModelAndView("page");		
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome",true);
+		return mv;				
 	}
 	
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {		
+		ModelAndView mv = new ModelAndView("page");		
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout",true);
+		return mv;				
+	}	
 	
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {		
+		ModelAndView mv = new ModelAndView("page");		
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact",true);
+		return mv;				
+	}		
 	
-	@RequestMapping("/test/{greeting}")
-	public ModelAndView pathtest(@PathVariable(value="greeting") String greeting) {
-	//	String greeting="bc";	
-		ModelAndView mv =new ModelAndView("page");
-		mv.addObject("greeting",greeting);
-		return mv;
-		
-	}
-
 }
